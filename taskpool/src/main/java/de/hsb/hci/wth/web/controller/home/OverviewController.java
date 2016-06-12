4f1032dd41d3be449461a47976324654a9e75b50
Vitmonk <tmb@live.de>
2016-06-12 18:46:43 +0200

    Fix sorting bug in overview

--- taskpool/src/main/java/de/hsb/hci/wth/web/controller/home/OverviewController.java
+++ taskpool/src/main/java/de/hsb/hci/wth/web/controller/home/OverviewController.java
@@ -1,7 +1,11 @@
 package de.hsb.hci.wth.web.controller.home;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
 
 import de.hsb.hci.wth.web.model.entities.Job;
 import de.hsb.hci.wth.web.model.persistance.JobRepository;
@@ -9,20 +13,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@ViewScoped
 public class OverviewController {
 
     @Autowired
     private JobRepository jobRepository;
 
-    public List<Job> getActiveJobs() {
-        return jobRepository.getAllJobs().stream()
+    private List<Job> activeJobs = new ArrayList<>();
+    private List<Job> openJobs = new ArrayList<>();
+
+    @PostConstruct
+    private void init() {
+        activeJobs = jobRepository.getAllJobs().stream()
                 .filter(job -> job.getCurrentDevice() != null)
+                .collect(Collectors.toList());
+        openJobs = jobRepository.getAllJobs().stream()
+                .filter(job -> job.getCurrentDevice() == null && !job.isLocked())
                 .collect(Collectors.toList());
     }
 
+    public List<Job> getActiveJobs() {
+        return activeJobs;
+    }
+
     public List<Job> getOpenJobs() {
-        return jobRepository.getAllJobs().stream()
-                .filter(job -> job.getCurrentDevice() == null && !job.isLocked())
-                .collect(Collectors.toList());
+        return openJobs;
     }
 }
